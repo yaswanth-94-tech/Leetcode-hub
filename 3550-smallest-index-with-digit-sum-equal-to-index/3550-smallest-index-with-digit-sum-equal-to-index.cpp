e556fa3a36456b6879cc463f3b8c5f3a6b4b6d0b
BOBBARI YASWANTH <yaswanthbobbari@gmail.com>
2026-09-24 19:49:41 +0530

    Time: 4 ms (2.54%), Space: 31.1 MB (15.11%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -1,15 +1,27 @@
-// optimized version
 class Solution {
 public:
-    int smallestIndex(vector<int>& nums) {
-        const int n=min(28, int(nums.size()));
-        for(int i=0; i<n; i++){
-            int x=nums[i], digitSum=0;
-            for(; x>0; x/=10){
-                digitSum+=x%10;
-            }
-            if (digitSum==i) return i;
+    int sod(int &num){
+        int sum=0;
+        while(num>0){
+            int rem=num%10;
+            sum=sum+rem;
+            num=num/10;
         }
-        return -1;
+        return sum;
+    }
+    int smallestIndex(vector<int>& nums) {
+        int ind=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            int sum=sod(nums[i]);
+            if(sum==i){
+                ind=min(ind,i);
+            }
+        }
+        if(ind==INT_MAX){
+            return -1;
+        }
+        else{
+            return ind;
+        }
     }
 };
